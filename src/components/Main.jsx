d0429cc5e44857e31b84e4b8798c8a6e5b4d7e65
Canyelles Elisa <elisa.canyelles@burda-forward.de>
2022-08-25 17:19:42 +0200

    react router native implemented

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar";
+import { Redirect, Route, Switch } from 'react-router-native'
 
 
 const Main = () => {
     return (
         <View style={{ flexGrow: 1 }}>
             <AppBar />
-            <RepositoryList />
+            <Switch>
+                <Route path='/' exact>
+                    <RepositoryList />
+                </Route>
+                <Route path='/signin' exact>
+                    <Text>Working on it</Text>
+                </Route>
+                <Redirect to='/' />
+            </Switch>
+
         </View>
     )
 }
