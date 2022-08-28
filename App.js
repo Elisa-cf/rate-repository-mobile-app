ae8a7e23e8251e1c206ee4c0ca80048778d1187c
Canyelles Elisa <elisa.canyelles@burda-forward.de>
2022-08-28 17:25:33 +0200

    add statusBar

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import Main from './src/components/Main'
 import { NativeRouter } from 'react-router-native'
-
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
-  return <NativeRouter>
-    <Main />
-  </NativeRouter>
+  return <>
+    <StatusBar style='light' />
+    <NativeRouter>
+      <Main />
+    </NativeRouter>
+  </>
   {/* <TouchableNativeFeedback onPress={() => Alert.alert('Yes, te amo!')}>
         <Text>Elodie I love you</Text>
       </TouchableNativeFeedback> */}
